e9c14f8d6b1423736fa880dd5a7391b2696c791e
XY56L7 <palmartin99@gmail.com>
2024-11-12 00:27:52 +0100

    gh pages

--- next.config.ts
+++ next.config.ts
@@ -3,5 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
-
+module.exports = {
+  output: 'export',
+  basePath: '/pal-martin-portfolio',
+  images: {
+    unoptimized: true,
+  },
+};
 export default nextConfig;
